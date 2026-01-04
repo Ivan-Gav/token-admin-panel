b6843bf10a9149dc5050cfd1cb539d204f7ee197
Ivan Gavrilin <ivan.gavrilin@inbox.ru>
2026-01-04 20:56:17 +0300

    chore: add rollup vizualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig, type PluginOption } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import scrollbarPlugin from "tailwind-scrollbar";
 import svgr from "vite-plugin-svgr";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import { visualizer } from "rollup-plugin-visualizer";
+
 import path from "node:path";
 
 // https://vite.dev/config/
@@ -18,12 +20,28 @@ export default defineConfig({
     tanstackRouter({
       routesDirectory: "./src/routes",
       generatedRouteTree: "./src/router/routeTree.gen.ts",
+      autoCodeSplitting: true,
     }),
+    visualizer({
+      emitFile: true,
+      filename: "bundlestats.html",
+    }) as PluginOption,
   ],
   base: "./",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (/\/react(?:-dom)?/.test(id)) {
+            return "vendor";
+          }
+        },
+      },
     },
   },
 });
