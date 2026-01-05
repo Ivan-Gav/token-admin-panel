387dddbf77f4b858b23ea07290046ef95d418b21
Ivan Gavrilin <116670798+Ivan-Gav@users.noreply.github.com>
2026-01-05 16:40:48 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,19 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
-// const BASE_URL = import.meta.env.VITE_BASE_URL;
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-// console.log("BASE_URL: ", BASE_URL);
+console.log("BASE_URL: ", BASE_URL);
 
 async function enableMocking() {
   const { worker } = await import("./api/mock/browser.ts");
-  // return await worker.start({
-  //   serviceWorker: {
-  //     url: BASE_URL
-  //       ? `/${BASE_URL}/mockServiceWorker.js`
-  //       : `/mockServiceWorker.js`,
-  //   },
-  // });
-  return await worker.start();
+  return await worker.start({
+    serviceWorker: {
+      url: BASE_URL
+        ? `/${BASE_URL}/mockServiceWorker.js`
+        : `/mockServiceWorker.js`,
+    },
+  });
 }
 
 enableMocking().then(() => {
